288ba87f1cb578e9ceef5f8ccee48ab0ec96a894
Marcos Gabriel Miller <marcosgabrielmiller@gmail.com>
2022-02-17 06:41:35 -0300

    Update conversor de sistemas numericos, solucionados errores y se agregaron colores

--- Python/matematica/convertidor_base/base.py
+++ Python/matematica/convertidor_base/base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
 class Base:
     """Contiene el alfabeto y convierte distintas bases"""
     def __init__(self):
@@ -9,39 +12,40 @@ class Base:
         self.size = len(self.alphabet)-1
 
     def toDecimal(self, number, base):
-        """Algoritmo para convertir de cualquier base a decimal"""
+        """Convierte cualquier base a decimal"""
         toString = ''
         sum = 0
         number = str(number)
         point = number.find('.')
         if point == -1:
-            index = len(str(int(number))) - 1
+            index = len(str(number)) - 1
         else:
             index = point-1
         for num in number:
             if num != '.':
                 num = self.alphabet.index(num)
                 if (index == 0 and point == -1) or index == -1 * len(number[point+1:]):
-                    toString += f' {self.alphabet[num]}({base}^{index})'
+                    toString += f'{self.alphabet[num]}({base}^{index})'
                 else:
-                    toString += f' {self.alphabet[num]}({base}^{index}) +'
+                    toString += f'{self.alphabet[num]}({base}^{index}) + '
                 sum += (num * base**index)
                 index -= 1
-        print(toString)
+        print(f'{Fore.CYAN}{toString}')
         self.newBase = str(sum)
 
     def toBase(self, quotient, base):
-        """Algoritmo iterativo para convertir de decimal a cualquier base"""
+        """Convierte decimal a cualquier base"""
         iquotient = int(float(quotient))
         fquotient = float(quotient) - iquotient
+        counter = 0
         while(iquotient > 0):
             remainder = iquotient % base
-            print(f'{iquotient} = {iquotient // base}({base}) + {self.alphabet[remainder]}')
+            print(f'{Fore.LIGHTBLUE_EX}{iquotient} = {iquotient // base}({base}) + {self.alphabet[remainder]}')
             iquotient //= base
             self.newBase += self.alphabet[remainder]
+            counter += 1
         self.newBase = self.newBase[::-1]
         if fquotient != 0:
-            print('\n'*2)
             counter = 0
             self.newBase += '.'
             while(fquotient != 0 and counter < 20):
@@ -50,6 +54,6 @@ class Base:
                     self.newBase += self.alphabet[int(fquotient * base)]
                 else:
                     self.newBase += '0'
-                print(f'{round(fquotient, 5)}({base}) = {round(fquotient * base, 4)}')
+                print(f'{Fore.LIGHTCYAN_EX}{round(fquotient, 5)}({base}) = {round(fquotient * base, 4)}')
                 fquotient *= base
                 counter += 1
--- Python/matematica/convertidor_base/main.py
+++ Python/matematica/convertidor_base/main.py
@@ -3,39 +3,45 @@
 
 from base import Base
 import sys
+import colorama
+from colorama import Fore
 
 if __name__ == '__main__':
+    colorama.init(autoreset=True)
     singleton = Base()
 
-    def isOK(num, numBase, newBase):
+    def isOK(num, numBase:int, newBase:int):
         flag = True
         if not(newBase <= singleton.size and newBase >= 2 and numBase >= 2 and numBase <= singleton.size):
             flag = False
-            print(f'Por favor ingrese una base entre [0-{singleton.size}]')
+            print(f'{Fore.RED}Por favor ingrese una base entre [0-{singleton.size}]')
+        elif(newBase != 10 and numBase != 10):
+            flag = False
+            print(f'{Fore.RED}Al menos una de las bases debe ser 10')
+
         else:
             abc = '.'
             abc += singleton.alphabet[:numBase]
             for item in str(num):
                 if item not in abc:
                     flag = False
-                    print(f'Por favor ingrese un numero correcto')
+                    print(f'{Fore.RED}Por favor ingrese un numero correcto')
                     break
         return flag
 
     try:
-        num = input("Ingrese un numero\n")
-        numBase = int(input("Ingrese su base\n"))
-        newBase = int(input("Por favor ingrese la base a la que desee convertir\n"))
-        print('\n'*2)
+        print(f'{Fore.LIGHTYELLOW_EX}\nPrograma para convertir numeros de cualquier base a decimal o viceversa')
+        num = input(Fore.GREEN + 'Ingrese un numero\n')
+        numBase = int(input(Fore.GREEN + 'Ingrese su base\n'))
+        newBase = int(input(Fore.GREEN + 'Por favor ingrese la base a la que desee convertir\n'))
         assert (isOK(num, numBase, newBase))
     except AssertionError:
-        print('\nPor favor ingrese los valores nuevamente\n')
+        print(f'{Fore.RED}Por favor ingrese los valores nuevamente')
     except:
-        print(f"Unexpected error: {sys.exc_info()[0]}")
+        print(f'{Fore.RED}Unexpected error: {sys.exc_info()[0]}')
     else:
         if newBase == 10:
             singleton.toDecimal(num, numBase)
         else:
             singleton.toBase(num, newBase)
-        print('\n'*2)
-        print(singleton.newBase)
+        print(f'{Fore.MAGENTA}{singleton.newBase}')
