39f099c69e11c3eb558a2659c89d17c447172bf6
Marcos Gabriel Miller <marcosgabrielmiller@gmail.com>
2023-03-22 21:45:10 -0300

    Probando otros parametros

--- Python/msc/chatgpt/chatgpt.py
+++ Python/msc/chatgpt/chatgpt.py
@@ -18,22 +18,28 @@ def main():
         completion = openai.Completion.create(
             engine=config["engine"],
             prompt=config["prompt"],
-            max_tokens=config["max_tokens"]
+            max_tokens=config["max_tokens"],
+            # logit_bias=config["logit_bias"],
+            temperature=config["temperature"]
             )
     
         print( f"{Fore.WHITE}{completion.choices[0].text}" )
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
     config = {
         "engine": "text-davinci-003",
         "prompt": "",
         "max_tokens": 2048,
+        "temperature" : 0,
+        # "logit_bias": 100,
         "api_key": os.environ.get('API_KEY')
     }
 
-    load_dotenv()
-
     openai.api_key = config["api_key"]
+
+    # print( openai.Model.list() )
 
     main()
